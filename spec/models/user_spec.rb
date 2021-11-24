27bde7c9833296bfc8e3200651385a09a4216127
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-11-25 07:36:14 +0900

    beforeで記述まとめ

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe "ユーザー新規登録" do
     it "nicknameが空では登録できない" do
-      user = FactoryBot.build(:user)
-      user.nickname = ""
-      user.valid?
+      @user.nickname = ""
+      @user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "emailが空では登録できない" do
-      user = FactoryBot.build(:user)
-      user.email = ""
-      user.valid?
+      @user.email = ""
+      @user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
   end
