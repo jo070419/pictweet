ee074e8e58105d33a1d506d05fc76ddcf2845aa9
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-11-25 07:39:46 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe User, type: :model do
     it "nicknameが空では登録できない" do
       @user.nickname = ""
       @user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "emailが空では登録できない" do
       @user.email = ""
       @user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
   end
 end
