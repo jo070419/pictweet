a0c59f01f41336dc455f1a79b78b3d8df99f8911
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-11-27 23:50:02 +0900

    example記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
 
   describe "ユーザー新規登録" do
     it "nicknameとemail,passwordとpassword_confirmationが存在すれば登録できる" do
+      expect(@user).to be_valid
     end
     it "nicknameが空では登録できない" do
     end
