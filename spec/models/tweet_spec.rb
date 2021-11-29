00f0b4e0e99339ce4c901628c2a3900747d1d8da
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-11-29 23:07:03 +0900

    ツイートが投稿できる場合のテスト記述

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tweet = FactoryBot.build(:tweet)
+  end
+
+  describe "ツイートの保存" do
+    context "ツイートが投稿できる場合" do
+      it "画像とテキストを投稿できる" do
+        expect(@tweet).to be_valid
+      end
+      it "テキストのみで投稿できる" do
+        @tweet.image = ""
+        expect(@tweet).to be_valid
+      end
+    end
+    context "ツイートが保存できない場合" do
+      it "テキストが空では投稿できない" do
+      end
+      it "ユーザーが紐付いてなければ投稿できない" do
+      end
+    end
+  end
 end
